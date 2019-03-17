10d49a335bf9eff081b04669c7e9bb921b2a78bd
prashanth-g <prashanthcsgce@gmail.com>
2019-03-17 10:40:23 +0530

    Refactored and added ServerApplication

---
+++ server/src/main/java/com/prashanth/spring/geode/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring.geode.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+
+@SpringBootApplication
+@CacheServerApplication(locators = "localhost[10334]")
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
