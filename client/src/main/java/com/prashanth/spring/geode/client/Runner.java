8005904ecc3ed67b454de9bbf9621c83d109136c
prashanth-g <prashanthcsgce@gmail.com>
2019-03-25 22:45:30 +0530

    Updated Runner to get cluseter avearge temp on ready event

--- client/src/main/java/com/prashanth/spring/geode/client/Runner.java
+++ client/src/main/java/com/prashanth/spring/geode/client/Runner.java
@@ -1,33 +1,45 @@
 package com.prashanth.spring.geode.client;
 
 import com.prashanth.spring.geode.common.Temperature;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Component
+@Log4j2
 public class Runner implements ApplicationListener<ApplicationReadyEvent> {
 
     private final TemperatureRepository temperatureRepository;
+    private final AverageTemperatureFunctionInvoker averageTemperatureFunctionInvoker;
 
-    public Runner(TemperatureRepository temperatureRepository) {
+    public Runner(TemperatureRepository temperatureRepository, AverageTemperatureFunctionInvoker averageTemperatureFunctionInvoker) {
         this.temperatureRepository = temperatureRepository;
+        this.averageTemperatureFunctionInvoker = averageTemperatureFunctionInvoker;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        // cleanup existing data
+        // Cleanup existing data
         this.temperatureRepository
                 .findAll()
                 .forEach(this.temperatureRepository::delete);
 
-        // insert new record
+        // Insert new record
         int randomOffset = new Random().nextInt(1000);
         for (int i = 0; i < randomOffset+100; i++) {
             this.insert();
         }
+
+        // Get Cluster average
+        Collection<Double> averages = this.averageTemperatureFunctionInvoker.averageTemperature();
+        Double clusterAverageTemp = averages.stream().collect(Collectors.averagingDouble(t -> t));
+
+        log.info("Cluster average "+ clusterAverageTemp);
     }
 
     private void insert() {
